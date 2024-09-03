1b4e6f105a4447ef4b4fda73a3dc888b7e012ff5
ArpitaPolara <arpitapolara25@gmail.com>
2024-09-02 19:11:58 -1200

    chnges

--- crud.py
+++ crud.py
@@ -2,11 +2,11 @@ import mysql.connector as c
 con=c.connect(host="localhost",user="root",password="root",database="biawork")
 cursor=con.cursor()
 
-# courser.execute("create table tblcust(cid int primary key,cname varchar(30),quantity int,price int)")
+# cursor.execute("create table tblcust(cid int primary key,cname varchar(30),quantity int,price int)")
 # con.close()
 
 # insert data
-while True:
+def insert_data():
     cid=int(input("Enter Customer Id:"))
     cname=input("Enter Customer Name:")
     quantity=int(input("Enter Quntity:"))
@@ -15,35 +15,66 @@ while True:
     cursor.execute(query)
     con.commit()
     print("Data Inserted Successfully..")
-    x=int(input("1->Enter more\n2->Exit\nEnter choice:"))
-    if x==2:
-        break 
+    
+
 
 # update data
-cid=int(input("Enter Customer Id:"))
-quantity=int(input("Enter Quntity:"))
-query="update tblcust set quantity={} where cid={}".format(cid,quantity)
-cursor.execute(query)
-con.commit()
-if cursor.rowcount>0:
-    print("Data Updated Successfully..")
-else:
-    print("No Data Found.")
+def update_data():
+    cid=int(input("Enter Customer Id:"))
+    quantity=int(input("Enter Quntity:"))
+    query="update tblcust set quantity={} where cid={}".format(cid,quantity)
+    cursor.execute(query)
+    con.commit()
+    if cursor.rowcount>0:
+        print("Data Updated Successfully..")
+    else:
+        print("No Data Found.")
+
+
 
 
 # delete data
-cid=int(input("Enter Customer Id:"))
-query="delete from tblcust where cid={}".format(cid)
-cursor.execute(query)
-con.commit()
-if cursor.rowcount>0:
-    print("Deletion successfull..")
-else:
-    print("Customer id not Found")
+def delete_data():
+    cid=int(input("Enter Customer Id:"))
+    query="delete from tblcust where cid={}".format(cid)
+    cursor.execute(query)
+    con.commit()
+    if cursor.rowcount>0:
+        print("Deletion successfull..")
+    else:
+        print("Customer id not Found")
+    
+
 
 # read data
-cursor.execute("select *from tblcust")
-record=cursor.fetchall()
-for i in record:
-    print(i)
-print("Total number of records=",cursor.rowcount)
+def read_data():
+    cursor.execute("select *from tblcust")
+    record=cursor.fetchall()
+    for i in record:
+         print(i)
+    print("Total number of records=",cursor.rowcount)
+    
+
+
+while True:
+    print("\nOptions:")
+    print("1. Insert Data")
+    print("2. Read Data")
+    print("3. Update Data")
+    print("4. Delete Data")
+    print("5. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        insert_data()
+    elif choice == '2':
+        read_data()
+    elif choice == '3':
+        update_data()
+    elif choice == '4':
+        delete_data()
+    elif choice == '5':
+        break
+    else:
+        print("Invalid choice. Please try again.")
